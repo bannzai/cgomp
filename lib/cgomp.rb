c6257df3362838e5ab3f9f0ac68eb5648a7a2bd4
bannzai <kingkong999yhirose@gmail.com>
2020-05-15 19:27:58 +0900

    :construction: Implement parser

--- lib/cgomp.rb
+++ lib/cgomp.rb
@@ -1,6 +1,13 @@
 require "cgomp/version"
+require "cgomp/reader"
+require "cgomp/parser"
 
 module Cgomp
   class Error < StandardError; end
   # Your code goes here...
+  def self.greet
+    html = Reader.new("#{Dir.getwd}/testdata/test.md").read
+    hash = Parser.new(html).parse
+    hash
+  end
 end
