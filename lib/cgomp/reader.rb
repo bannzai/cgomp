524ba74e8912f4e0a1549aa98aed8a0fce4a1bf6
bannzai <kingkong999yhirose@gmail.com>
2020-05-15 19:27:15 +0900

    :recycle: Using redcarpet

--- lib/cgomp/reader.rb
+++ lib/cgomp/reader.rb
@@ -1,4 +1,4 @@
-require 'kramdown'
+require 'redcarpet'
 
 class Reader
   def initialize(filepath)
@@ -6,10 +6,22 @@ class Reader
   end
 
   def read 
-    Kramdown::Document.new(content, {parse_block_html: true}).to_html
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(content)
   end
 
   def content
     File.read(@filepath)
+  end
+  def extensions
+    {
+      fenced_code_blocks: true
+    }
+  end
+  def render_options
+    {
+      escape_html: true
+    }
   end
 end
